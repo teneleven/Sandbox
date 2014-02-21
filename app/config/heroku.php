d5291f6b45e76506ac22dd3e5fe6a1f02a9b89fb
Justin Hilles <justin@1011i.com>
2014-02-21 09:39:48 -0800

    add heroku parameters file

---
+++ app/config/heroku.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_ENV['DATABASE_URL'])) {
+    $dbUrl = $_ENV['DATABASE_URL'];
+    $parts = parse_url($dbUrl);
+
+    $container->setParameter('database_driver', 'pdo_pgsql');
+    $container->setParameter('database_host', $parts['host']);
+    $container->setParameter('database_name', trim($parts['path'], '/'));
+    $container->setParameter('database_user', $parts['user']);
+    $container->setParameter('database_password', $parts['pass']);
+    $container->setParameter('database_port', $parts['port']);
+}
+
+if (isset($_ENV['SECRET'])) {
+    $container->setParameter('secret', $_ENV['SECRET']);
+}
